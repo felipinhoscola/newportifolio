e1fb885a921d1529d25cdfa5d8d096597a4d31c5
Felipe Scola <felipescola0@gmail.com>
2023-08-14 15:14:13 -0300

    ajustes

--- components/habilidades/habilidades.jsx
+++ components/habilidades/habilidades.jsx
@@ -1,6 +1,5 @@
 import styled from "styled-components";
 import css from "./habilidades.module.scss";
-import FundoExp from "./../objects/fundoExp";
 import Image from "next/image";
 import { useState } from "react";
 
