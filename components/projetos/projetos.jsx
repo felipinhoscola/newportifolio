35168b0573f3d8ef5f8918a3d68ca94b584a9fad
Felipe Scola <felipescola0@gmail.com>
2023-08-16 22:14:37 -0300

    obs projeto

--- components/projetos/projetos.jsx
+++ components/projetos/projetos.jsx
@@ -1,5 +1,6 @@
 import css from "./projetos.module.scss";
-
+// varios cards com projetos, contendo uma breve desc e o link para github, no estilo grid um do lado do outro, fundo de vidro
+// passa o mouse em cima ele da uma crescida com uma logo no meio do card bem basica, fazer usando string para conseguir adicionar mais projetos
 const Projetos = () =>{
     return (
         <div className={css.container}>
