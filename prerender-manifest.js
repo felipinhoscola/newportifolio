d251cf8cc48f34a20bf9a696c2ca637d6b601ce6
Felipe Scola <felipescola0@gmail.com>
2023-08-23 20:18:35 -0300

    Updates

---
+++ prerender-manifest.js
@@ -0,0 +1,1 @@
+self.__PRERENDER_MANIFEST={"version":4,"routes":{},"dynamicRoutes":{},"preview":{"previewModeId":"e9b871e5c84ba9299bdec074ec712b2a","previewModeSigningKey":"2e8175214e53088ffa981375f2683e196ab5a76b2056451406d3c24686b0e0f2","previewModeEncryptionKey":"f9a2b32ffebf46c12a7603d636ec4029f96837798263d17b2f661e666eb98372"},"notFoundRoutes":[]}
